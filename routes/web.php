730b8405fe82f23be71df8a5ff3b0892bd5c86f2
oki <oki.hartanto@gmail.com>
2022-12-27 17:27:13 +0700

    Route

--- routes/web.php
+++ routes/web.php
@@ -585,8 +585,8 @@ Route::group( ['middleware' => ['auth']], function() {
 	})->name('database.backup');
 
 	Route::get('git-pull', function (Request $request) {
-		Artisan::call('app:git_pull');
-		$message  = "Git Pull Success";
+		$message = Artisan::call('app:git_pull');
+		// $message  = "Git Pull Success";
 		\LogActivity::addToLog('Command',"Jobs $message");
 		return redirect()->back()->with('success',$message); 
 	})->name('git.pull');
