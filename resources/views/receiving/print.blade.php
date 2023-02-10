e232155d79709c84c780399618d6277bc90d8ffb
oki <oki.hartanto@gmail.com>
2023-02-10 10:12:25 +0700

    Print size untuk LPB

--- resources/views/receiving/print.blade.php
+++ resources/views/receiving/print.blade.php
@@ -12,8 +12,8 @@
             /*size: 210mm 94mm; 
             margin-left: 20mm; 
             margin-top: 20mm; */
-            /* size: A4; */
-            size: 210mm 210mm;
+            size: A5;
+            /* size: 148.5mm 210mm; */
             margin:0;
             mergin-left:10px;
         }
@@ -25,7 +25,7 @@
         * For screen preview *
         @media screen {
             body { 
-                width: 210mm; 
+                width: 148.5mm; 
                 height: 210mm;
                 margin:0;
             }
@@ -41,8 +41,8 @@
             {
                 /* size: 8.5in 11in;  */
                 /* size: 8.5in 6in;  */
-                /* size: A5; */
-                size: 210mm 210mm ;
+                size: A5;
+                /* size: 148.5mm 210mm ; */
                 /* margin: 0 !important; */
                 margin-left:10px;
             }
@@ -50,7 +50,7 @@
             p { page-break-after: always; }
 
             html, body { 
-                height: 210mm;
+                height: 148.5mm;
                 width: 210mm;
                 margin:0 !important;
             }
@@ -60,7 +60,7 @@
             /* font-family: Courier New,Courier,Lucida Sans Typewriter,Lucida Typewriter,monospace;  */
             /* font-family: Calibri,Arial, Helvetica, sans-serif; */
             /* background-color: aqua; */
-            height: 210mm;
+            height: 148.5mm;
             width: 210mm;
         } 
         
