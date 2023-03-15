642d77f7bc1a06c9ba5bece71b29715de01c2658
oki <oki.hartanto@gmail.com>
2023-03-15 10:16:56 +0700

    Print Delivery

--- resources/views/delivery/print.blade.php
+++ resources/views/delivery/print.blade.php
@@ -55,7 +55,7 @@
         }
 
         .header, .header-space{
-                height: 215px;
+                height: 190px;
         }
 
         .footer, .footer-space {
@@ -166,6 +166,11 @@
         .tanpa-padding{
             padding:0px;
         }
+
+        .huruf-tebal{
+            font-weight: bold;
+        }
+
     </style>
 </head>
 <body class="{{ (count($details)) < 7 ? "A4A5" : "A4" }}">
@@ -173,140 +178,141 @@
     <table>
         <thead>
             <tr>
-            <td>
-            <div class="header-space">
-            <br>
-            <table width="100%" style="border: 1px solid #0c0c0c;padding-left:10px">
-                <tr>
-                    <td width="30%" class="font-12 " >
-                        <img src="{{ asset('app-assets/images/logo/logo_po.png') }}" alt="logo" style="width: 60%;"> 
-                    </td>
-                    <td  width="50%" >
-                        <h3 class="padding:0px;">DELIVERY NOTE</h3>
-                        <table>
-                            <tr class="tanpa-padding">
-                                <td class="font-14 tanpa-padding">Nomor</td>
-                                <td class="font-14 tanpa-padding">: {{ $dnHdr->delivery_number }}</td>
-                            </tr>
-                            <tr class="tanpa-padding">
-                                <td class="font-14 tanpa-padding">No.PO#</td>
-                                <td class="font-14 tanpa-padding">: {{ $dnHdr->po_number }}</td>
+                <td>
+                    <div class="header-space">
+                        <br>
+                        <table width="100%" style="border: 1px solid #0c0c0c;padding-left:10px">
+                            <tr>
+                                <td width="30%" class="font-12 " >
+                                    <img src="{{ asset('app-assets/images/logo/logo_po.png') }}" alt="logo" style="width: 60%;"> 
+                                </td>
+                                <td  width="50%" >
+                                    {{-- <h3 class="padding:0px;">DELIVERY NOTE</h3> --}}
+                                    <div class="huruf-tebal">DELIVERY NOTE</div>
+                                    <table>
+                                        <tr class="tanpa-padding">
+                                            <td class="font-14 tanpa-padding">Nomor</td>
+                                            <td class="font-14 tanpa-padding">: {{ $dnHdr->delivery_number }}</td>
+                                        </tr>
+                                        <tr class="tanpa-padding">
+                                            <td class="font-14 tanpa-padding">No.PO#</td>
+                                            <td class="font-14 tanpa-padding">: {{ $dnHdr->po_number }}</td>
+                                        </tr>
+                                    </table>
+                                </td>
                             </tr>
                         </table>
-                    </td>
-                </tr>
-            </table>
-            <table style="border-left: 1px solid #0c0c0c;border-right: 1px solid #0c0c0c;padding-left:10px" class="font-14 tanpa-padding">
-                <tr>
-                    <td width="50%" valign="top">
-                        <table>
+                        <table style="border-left: 1px solid #0c0c0c;border-right: 1px solid #0c0c0c;padding-left:10px" class="font-14 tanpa-padding">
                             <tr>
-                                <td width="25%" class="tanpa-padding">SO Number</td><td class="tanpa-padding">: {{ $dnHdr->so_number }}</td>
-                            </tr>
-                            <tr>
-                                <td class="tanpa-padding">Tanggal</td><td class="tanpa-padding">: {{ $dnHdr->delivery_date }}</td>
-                            </tr>
-                            <tr>
-                                <td class="tanpa-padding">Jam</td><td class="tanpa-padding">: {{ date('H:i:s') }}</td>
-                            </tr>
-                            <tr>
-                                <td class="tanpa-padding">No Mobil</td><td class="tanpa-padding">:</td>
+                                <td width="50%" valign="top">
+                                    <table>
+                                        <tr>
+                                            <td width="25%" class="tanpa-padding">SO Number</td><td class="tanpa-padding">: {{ $dnHdr->so_number }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td class="tanpa-padding">Tanggal</td><td class="tanpa-padding">: {{ $dnHdr->delivery_date }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td class="tanpa-padding">Jam</td><td class="tanpa-padding">: {{ date('H:i:s') }}</td>
+                                        </tr>
+                                        <tr>
+                                            <td class="tanpa-padding">No Mobil</td><td class="tanpa-padding">:</td>
+                                        </tr>
+                                    </table>
+                                </td>
+                                <td width="50%" valign="top" style="border-left: 1px solid #0c0c0c;padding-left:5px" >
+                                    <strong>Kepada Yth.</strong><br>
+                                        {{ $customers->nama }} <br>
+                                        {{ $customers->alamat_kirim_1 }} <br>
+                                    
+                                </td>
                             </tr>
                         </table>
-                    </td>
-                    <td width="50%" valign="top" style="border-left: 1px solid #0c0c0c;padding-left:5px" >
-                        <strong>Kepada Yth.</strong><br>
-                            {{ $customers->nama }} <br>
-                            {{ $customers->alamat_kirim_1 }} <br>
-                        
-                    </td>
-                </tr>
-            </table>
-            </div>
-            </td>
+                    </div>
+                </td>
             </tr>
         </thead>
         <tbody>
             <tr>
-            <td>
-            <div class="content">
-                <table id="tblContent" class="font-14">
-                    <thead style="background-color: lightgray;">
-                    <tr>
-                        <th width="5%" class="border-header">No</th>
-                        <th width="15%" class="border-header">Code</th>
-                        <th width="60%" class="border-header">Description</th>
-                        <th width="10%" class="border-header">Qty</th>
-                        <th width="10%" class="border-header">UOM</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                        
-                        @foreach ($details as $val )
-                            <tr >
-                                <td align="center">{{ ++$no }}</td>
-                                <td align="left">{{ $val->article_alternative_code }}</td>
-                                <td align="left">{{ $val->article_desc }}</td>
-                                <td align="right">{{ number_format($val->qty) }}</td>
-                                <td align="left">{{ $val->uom }}</td>
+                <td>
+                    <div class="content">
+                        <table id="tblContent" class="font-14">
+                            <thead style="background-color: lightgray;">
+                            <tr>
+                                <th width="5%" class="border-header">No</th>
+                                <th width="15%" class="border-header">Code</th>
+                                <th width="60%" class="border-header">Description</th>
+                                <th width="10%" class="border-header">Qty</th>
+                                <th width="10%" class="border-header">UOM</th>
                             </tr>
-                        @endforeach      
-                                             
-                        
-                        @if(count($details)>7)
-                            <?php $totalBaris = 16 ?>
-                        @else
-                            <?php $totalBaris = 7 ?>
-                        @endif
+                            </thead>
+                            <tbody>
+                                
+                                @foreach ($details as $val )
+                                    <tr >
+                                        <td align="center">{{ ++$no }}</td>
+                                        <td align="left">{{ $val->article_alternative_code }}</td>
+                                        <td align="left">{{ $val->article_desc }}</td>
+                                        <td align="right">{{ number_format($val->qty) }}</td>
+                                        <td align="left">{{ $val->uom }}</td>
+                                    </tr>
+                                @endforeach      
+                                                    
+                                
+                                @if(count($details)>7)
+                                    <?php $totalBaris = 16 ?>
+                                @else
+                                    <?php $totalBaris = 7 ?>
+                                @endif
 
-                        @for ($i=1;$i< $totalBaris-(count($details));$i++)
-                            <tr >
-                                <td align="right" class="putih" height="20"></td>
-                                <td align="left"></td>
-                                <td align="left"></td>
-                                <td align="right"></td>
-                                <td align="left"></td>
+                                @for ($i=1;$i< $totalBaris-(count($details));$i++)
+                                    <tr >
+                                        <td align="right" class="putih" height="20"></td>
+                                        <td align="left"></td>
+                                        <td align="left"></td>
+                                        <td align="right"></td>
+                                        <td align="left"></td>
+                                    </tr>
+                                @endfor
+                                                        
+                                <tr style="border: 1px solid #0c0c0c;padding-left:10px">
+                                    <td align="right" colspan="3" style="border-right:none">Total Qty :</td>
+                                    <td align="right" style="border-left:none;border-right:none"> {{ number_format($totals[0]->qty) }}</td>
+                                    <td style="border-left:none"></td>
+                                </tr>
+                                
+                                <tr>
+                                    <td colspan="5">Note: </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                        <table width="100%">
+                            <tr><td colspan="5" height="10"></td></tr>
+                            <tr>
+                                <td align="center">Created By</td>
+                                <td align="center">Checked By</td>
+                                <td align="center">Shipped By</td>
+                                <td align="center">Security By</td>
+                                <td align="center">Received By</td>
                             </tr>
-                        @endfor
-                                                
-                        <tr style="border: 1px solid #0c0c0c;padding-left:10px">
-                            <td align="right" colspan="3" style="border-right:none">Total Qty :</td>
-                            <td align="right" style="border-left:none;border-right:none"> {{ number_format($totals[0]->qty) }}</td>
-                            <td style="border-left:none"></td>
-                        </tr>
-                        
-                        <tr>
-                            <td colspan="5">Note: </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <table width="100%">
-                    <tr><td colspan="5" height="10"></td></tr>
-                    <tr>
-                        <td align="center">Created By</td>
-                        <td align="center">Checked By</td>
-                        <td align="center">Shipped By</td>
-                        <td align="center">Security By</td>
-                        <td align="center">Received By</td>
-                    </tr>
-                    <tr>
-                        <td align="center" height="20"></td>
-                        <td align="center"></td>
-                        <td align="center"></td>
-                        <td align="center"></td>
-                        <td align="center"></td>
-                    </tr>
-                    <tr>
-                        {{-- <td align="center">(<u>{{ str_pad($dnHdr->created_by, 10, "_", STR_PAD_BOTH)  }}</u>)</td> --}}
-                        <td align="center">( _________ )</td>
-                        <td align="center">( _________ )</td>
-                        <td align="center">( _________ )</td>
-                        <td align="center">( _________ )</td>
-                        <td align="center">( _________ )</td>
-                    </tr>
-                </table>
-            </div>
-            </td>
+                            <tr>
+                                <td align="center" height="20"></td>
+                                <td align="center"></td>
+                                <td align="center"></td>
+                                <td align="center"></td>
+                                <td align="center"></td>
+                            </tr>
+                            <tr>
+                                {{-- <td align="center">(<u>{{ str_pad($dnHdr->created_by, 10, "_", STR_PAD_BOTH)  }}</u>)</td> --}}
+                                <td align="center">( _________ )</td>
+                                <td align="center">( _________ )</td>
+                                <td align="center">( _________ )</td>
+                                <td align="center">( _________ )</td>
+                                <td align="center">( _________ )</td>
+                            </tr>
+                        </table>
+                    </div>
+                </td>
             </tr>
         </tbody>
         <tfoot>
@@ -320,17 +326,17 @@
     </table>
 </div>
 <script>
-    window.onload= function () {
-        window.print();
-        window.onafterprint = function () {
-            window.close();
-        }
-        window.onfocus = function () { 
-            setTimeout(function () { 
-                window.close(); 
-            }, 200); 
-        }
-    }
+    // window.onload= function () {
+    //     window.print();
+    //     window.onafterprint = function () {
+    //         window.close();
+    //     }
+    //     window.onfocus = function () { 
+    //         setTimeout(function () { 
+    //             window.close(); 
+    //         }, 200); 
+    //     }
+    // }
 </script>
 </body>
 </html>
