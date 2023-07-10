c6f69edbd782e801e59d4bccd28b2ccda4c97be9
oki <oki.hartanto@gmail.com>
2023-07-10 15:16:19 +0700

    Perbaikan detail PO V.4.9.0.7

--- resources/views/purchaseOrder/index.blade.php
+++ resources/views/purchaseOrder/index.blade.php
@@ -177,7 +177,7 @@
     }
     showDataTables({
       tableId:"detailedTable",
-      route:"{{ route('purchaseOrder.list.detail') }}",
+      route:"{{ route('purchaseOrder.listDetail') }}",
       kolom:{!! $kolomDetail !!},
       // arrColPrint:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27],
       // columnDefs :[
