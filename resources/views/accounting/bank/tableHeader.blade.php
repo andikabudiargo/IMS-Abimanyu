37e2e786ea4238c59dfe51af6feed73b422fc0dd
oki <oki.hartanto@gmail.com>
2023-07-16 10:12:29 +0700

    commit initial

---
+++ resources/views/accounting/bank/tableHeader.blade.php
@@ -0,0 +1,27 @@
+<table class="" style="width:98%;table-layout: fixed;">
+    <tbody>
+        <tr>
+            <td class="isian" style="width: 20%">
+                <label>Account</label>
+            </td>
+            <td class="isian" style="width: 25%">
+                <label>Description</label>
+            </td>
+            <td class="isian" style="">
+                <label>Referensi</label>
+            </td>
+            <td class="isian" style="">
+                <label>CC</label>
+            </td>
+            <td class="isian" style="width: 10%">
+                <label>Debit</label>
+            </td>
+            <td class="isian" style="width: 10%">
+                <label>Credit</label>
+            </td>
+            <td class="isian" style="width: 5%">
+                <label>-</label>
+            </td>
+        </tr>
+    </tbody>
+</table>
