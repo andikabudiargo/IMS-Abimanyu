552ec929107fede29d6bcd4ff6bf850a621d6cab
oki <oki.hartanto@gmail.com>
2023-10-26 15:02:37 +0700

    Index Ap ditambah approval by

--- app/Http/Controllers/AccountPayableController.php
+++ app/Http/Controllers/AccountPayableController.php
@@ -84,6 +84,8 @@ class AccountPayableController extends Controller
             ['data'=> 'total_discount', 'name'=> 'total_discount','title'=>'Discount'],
             ['data'=> 'grand_total', 'name'=> 'grand_total','title'=>'Grand Total'],
             ['data'=> 'note', 'name'=> 'note','title'=>'Note'],
+            ['data'=> 'approval_by','name'=> 'approval_by','title'=>'Approved By'],
+            ['data'=> 'approval_at','name'=> 'approval_at','title'=>'Approved At'],
             ['data'=> 'created_by', 'name'=> 'created_by','title'=>'Created By'],
             ['data'=> 'created_at', 'name'=> 'created_at','title'=>'Created At']
         ];
@@ -1504,6 +1506,8 @@ class AccountPayableController extends Controller
             'ap_invoice.*'
             ,DB::raw("(select STRING_AGG ( a.rec_number,',' ORDER BY a.id) as list_rec from ap_invoice_detail a where ap_number = ap_invoice.ap_number) as list_rec")
             ,db::raw("concat(third_party.kode,'-',third_party.nama) as supplier_name")
+            ,db::raw("(select (select name from users where username = z.username) from approval_history z where module_number = ap_invoice.ap_number order by approval_order desc limit 1) as approval_by")
+            ,db::raw("(select to_char(approval_date::date, 'DD-MM-YYYY') from approval_history z where module_number = ap_invoice.ap_number order by approval_order desc limit 1) as approval_at")
         )
         ->orderBy('ap_invoice.id')
         ->get(); 
