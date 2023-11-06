46804f8de7a27be945833775efc734f40d9ec938
oki <oki.hartanto@gmail.com>
2023-11-06 09:50:39 +0700

    index

--- resources/views/accountPayable/index.blade.php
+++ resources/views/accountPayable/index.blade.php
@@ -148,7 +148,7 @@
       tableId:"detailedTable",
       route:"{{ route('ap.list') }}",
       kolom:{!! $kolom !!},
-      // arrColPrint:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22],
+      arrColPrint:[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22],
       columnDefs :[
         { width: '5%', targets: 0 },
         {
