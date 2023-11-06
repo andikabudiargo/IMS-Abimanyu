40d86f96569ada95a164866215757a81d98e4c6a
oki <oki.hartanto@gmail.com>
2023-11-06 09:20:20 +0700

    psoting all di non aktifkan

--- routes/web.php
+++ routes/web.php
@@ -381,7 +381,7 @@ Route::group( ['middleware' => ['auth']], function() {
 	Route::get('aps/notif/approve',['as'=>'ap.notif.approve','uses'=>'AccountPayableController@approve']);
 	Route::get('aps/print/draft',['as'=>'ap.print.draft','uses'=>'AccountPayableController@printDraft']);
 
-	Route::get('ap/posting/all',['as'=>'ap.posting.all','uses'=>'AccountPayableController@prosesAllPosting']);
+	// Route::get('ap/posting/all',['as'=>'ap.posting.all','uses'=>'AccountPayableController@prosesAllPosting']);
 
 	// Route::get('receivings/search',['as'=>'ap.search','uses'=>'ReceivingController@search']);
 	// Route::get('receivings/list/po',['as'=>'ap.list.po','uses'=>'ReceivingController@listPo']);
