0ef87c218bf88fca2379cd976c4101ba930a3464
oki <oki.hartanto@gmail.com>
2023-11-09 09:09:15 +0700

    index rece

--- resources/views/receiving/index.blade.php
+++ resources/views/receiving/index.blade.php
@@ -215,7 +215,12 @@
       arrColPrint:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18],
       columnDefs :[
         { width: '5%', targets: 0 },
-        { className: 'text-right','targets': [9,10,12,13] },
+        {
+          targets: [ 11,12,13,14,15 ],
+          render: $.fn.dataTable.render.number(',', '.',2, ''),
+          className: "text-right"
+        },
+        
       ],
       dataSearch:  {
         searchRec:searchRec,
