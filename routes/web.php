af0b7080b502b592199196cc091a3b4029ee4bb9
Oki Hartanto <oki.hartanto@yogyagroup.com>
2024-06-06 10:26:40 +0700

    Penambahan tombol export ke excel BOM

--- routes/web.php
+++ routes/web.php
@@ -516,6 +516,7 @@ Route::group( ['middleware' => ['auth']], function() {
 	Route::get('boms/code/create',['as'=>'bom.code.create','uses'=>'BomController@articleCodeCreate']);
 	Route::get('boms/print',['as'=>'bom.print','uses'=>'BomController@print']);
 	Route::post('boms/revision',['as'=>'bom.revision','uses'=>'BomController@revision','middleware' => ['permission:bom-revision']]);
+	Route::get('bom/export',['as'=>'bom.export','uses'=>'BomController@exportBom']);
 
 	Route::get('bom/report',['as'=>'bom.report.index','uses'=>'BomReportController@index','middleware' => ['permission:bom-index']]);
 	Route::post('bom/report/list',['as'=>'bom.report.list','uses'=>'BomReportController@list']);
