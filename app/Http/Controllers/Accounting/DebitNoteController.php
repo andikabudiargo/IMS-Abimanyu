1957ab145902aff326b514e60127bf0e54591a90
Oki Hartanto <oki.hartanto@yogyagroup.com>
2024-11-21 14:30:54 +0700

    Untuk article tidak udah jadi BOM dulu, semua article bisa di keluarkan

--- app/Http/Controllers/Accounting/DebitNoteController.php
+++ app/Http/Controllers/Accounting/DebitNoteController.php
@@ -1669,12 +1669,12 @@ class DebitNoteController extends Controller
 
         $custCode = $request->custCode;
         $data= DB::table('article') 
-        ->whereIn('article.article_code', function($query) use ($custCode) {
-            $query->select('article_code')
-            ->from('bom_hdr') 
-            ->where('status','3')
-            ->where('customer',$custCode);
-        })
+        // ->whereIn('article.article_code', function($query) use ($custCode) {
+        //     $query->select('article_code')
+        //     ->from('bom_hdr') 
+        //     ->where('status','3')
+        //     ->where('customer',$custCode);
+        // })
         ->where('third_party',$custCode)
         ->where('article_type','FG')
         ->orderBy('article_desc')
