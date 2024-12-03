429dddb319a88b5d0a3f236408e254138bb953b1
Oki Hartanto <oki.hartanto@yogyagroup.com>
2024-12-03 14:48:41 +0700

    Perbaikan perhitungan total

--- resources/views/accounting/accountPayable/script.blade.php
+++ resources/views/accounting/accountPayable/script.blade.php
@@ -551,8 +551,8 @@
                                     <td  class="text-right" style="padding:0px 5px 0px 5px;"><input type="text" class="form-control-plaintext disabled-el" id="articleTotal" name="articleTotal[]" value="${humanizeNumber(parseFloat(result.detailRec[i].total).toFixed(2))}" style="text-align:right;" disabled/></td>
                                 </tr>`;
                             console.log(`Qty ${result.detailRec[i].article} : ${parseFloat(result.detailRec[i].qty)}`);
-                            grandTotalQty=parseFloat(grandTotalQty)+parseFloat(result.detailRec[i].qty);
-                            console.log('Grand Total :'+grandTotalQty);
+                            grandTotalQty+=parseFloat(result.detailRec[i].qty);
+                            console.log('Grand Total :'+parseFloat(grandTotalQty));
                         }                       
 
                         $("#listOfRec tbody").append(isiTabel);
@@ -567,7 +567,8 @@
                             recNumber += $(this).data('rec-number')+",";
                             sumQty += parseFloat($(this).data('sum-qty'));
                         });
-                        console.log('Grand Total Cek :'+sumQty);
+
+                        console.log('Grand Total :'+grandTotalQty);
                     }
 
                     $('#totalPO').val(humanizeNumber(result.summaryRec[0].total_amount_po));
