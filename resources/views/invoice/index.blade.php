5630986ed71dd1a5acc636120e2414da3c8bfd83
Oki Hartanto <oki.hartanto@yogyagroup.com>
2025-05-09 09:08:25 +0700

    Sending date ke exportnya 2 kali

--- resources/views/invoice/index.blade.php
+++ resources/views/invoice/index.blade.php
@@ -208,7 +208,7 @@
       tableId:"detailedTable",
       route:"{{ route('invoice.list') }}",
       kolom:{!! $kolom !!},
-      arrColPrint:[1,2,4,5,6,7,8,9,10,11,12,13,14,15,17,19,20,21,22,23,27,28,29],
+      arrColPrint:[1,2,4,5,6,7,8,9,10,11,12,13,14,16,18,20,21,22,23,27,28,29],
       columnDefs :[
         { width: '5%', targets: 0 },
         {
