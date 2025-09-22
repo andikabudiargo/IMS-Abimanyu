7deff1d104aac3853809e379b5b72c66948c56be
Oki Hartanto <oki.hartanto@yogyagroup.com>
2025-09-22 11:28:22 +0700

    Perbaikan download excel detail BOM

--- app/Exports/BomExport.php
+++ app/Exports/BomExport.php
@@ -157,177 +157,178 @@ class BomExport implements FromView,ShouldAutoSize,WithColumnFormatting,WithEven
         $barisAll='';
         $jumlahBaris=0;
 
-        $barisHeader = "<tr>
-                        <td>Bom Code</td>
-                        <td>Num Revision</td>
-                        <td>Article Finish Good</td>
-                        <td>Article Raw Material</td>
-                        <td>Customer</td>
-                        <td>Group Of Material</td>
-                        <td>Bom Header</td>
-                        <td>Part No</td>
-                        <td>Model</td>
-                        <td>Note</td>
-                        <td>Spray Booth 1</td>
-                        <td>Spray Booth 2</td>
-                        <td>Spray Booth 3</td>
-                        <td>Tone 1</td>
-                        <td>Tone 2</td>
-                        <td>Tone 3</td>
-                        <td>Tack 1</td>
-                        <td>Tack 2</td>
-                        <td>Tack 3</td>
-                        <td>Pass Rate 1</td>
-                        <td>Pass Rate 2</td>
-                        <td>Pass Rate 3</td>
-                        <td>Pass Trough 1</td>
-                        <td>Pass Trough 2</td>
-                        <td>Pass Trough 3</td>
-                        <td>Cycle Time Buffing 1</td>
-                        <td>Cycle Time Buffing 2</td>
-                        <td>Cycle Time Buffing 3</td>
-                        <td>Article Code 1</td>
-                        <td>Article Code 2</td>
-                        <td>Article Code 3</td>
-                        <td>Article Code 4</td>
-                        <td>Tone 1</td>
-                        <td>Tone 2</td>
-                        <td>Tone 3</td>
-                        <td>Tone 4</td>
-                        <td>Pos 1</td>
-                        <td>Pos 2</td>
-                        <td>Pos 3</td>
-                        <td>Pos 4</td>
-                        <td>Qty 1</td>
-                        <td>Qty 2</td>
-                        <td>Qty 3</td>
-                        <td>Qty 4</td>
-                        <td>Uom 1</td>
-                        <td>Uom 2</td>
-                        <td>Uom 3</td>
-                        <td>Uom 4</td>
-                        <td>Uom Con 1</td>
-                        <td>Uom Con 2</td>
-                        <td>Uom Con 3</td>
-                        <td>Uom Con 4</td>
-                        </tr>";
+        // $barisHeader = "<tr>
+        //                 <td>Bom Code</td>
+        //                 <td>Num Revision</td>
+        //                 <td>Article Finish Good</td>
+        //                 <td>Article Raw Material</td>
+        //                 <td>Customer</td>
+        //                 <td>Group Of Material</td>
+        //                 <td>Bom Header</td>
+        //                 <td>Part No</td>
+        //                 <td>Model</td>
+        //                 <td>Note</td>
+        //                 <td>Spray Booth 1</td>
+        //                 <td>Spray Booth 2</td>
+        //                 <td>Spray Booth 3</td>
+        //                 <td>Tone 1</td>
+        //                 <td>Tone 2</td>
+        //                 <td>Tone 3</td>
+        //                 <td>Tack 1</td>
+        //                 <td>Tack 2</td>
+        //                 <td>Tack 3</td>
+        //                 <td>Pass Rate 1</td>
+        //                 <td>Pass Rate 2</td>
+        //                 <td>Pass Rate 3</td>
+        //                 <td>Pass Trough 1</td>
+        //                 <td>Pass Trough 2</td>
+        //                 <td>Pass Trough 3</td>
+        //                 <td>Cycle Time Buffing 1</td>
+        //                 <td>Cycle Time Buffing 2</td>
+        //                 <td>Cycle Time Buffing 3</td>
+        //                 <td>Article Code 1</td>
+        //                 <td>Article Code 2</td>
+        //                 <td>Article Code 3</td>
+        //                 <td>Article Code 4</td>
+        //                 <td>Tone 1</td>
+        //                 <td>Tone 2</td>
+        //                 <td>Tone 3</td>
+        //                 <td>Tone 4</td>
+        //                 <td>Pos 1</td>
+        //                 <td>Pos 2</td>
+        //                 <td>Pos 3</td>
+        //                 <td>Pos 4</td>
+        //                 <td>Qty 1</td>
+        //                 <td>Qty 2</td>
+        //                 <td>Qty 3</td>
+        //                 <td>Qty 4</td>
+        //                 <td>Uom 1</td>
+        //                 <td>Uom 2</td>
+        //                 <td>Uom 3</td>
+        //                 <td>Uom 4</td>
+        //                 <td>Uom Con 1</td>
+        //                 <td>Uom Con 2</td>
+        //                 <td>Uom Con 3</td>
+        //                 <td>Uom Con 4</td>
+        //                 </tr>";
                                     
-        foreach($headers as $val){
+        // foreach($headers as $val){
             
-            $bom_code = $val->bom_code;
-            $num_revision = $val->num_revision;
-            $article_finish_good = $val->article_finish_good;
-            $article_raw_material = $val->article_raw_material;
-            $customer = $val->customer;
-            $group_of_material = $val->group_of_material;
-            $uom = $val->uom;
-            $part_no = $val->part_no;
-            $model = $val->model;
-            $note = $val->note;
-            $spray_booth_1 = $val->spray_booth_1;
-            $spray_booth_2 = $val->spray_booth_2;
-            $spray_booth_3 = $val->spray_booth_3;
-            $tone_1 = $val->tone_1;
-            $tone_2 = $val ->tone_2;
-            $tone_3 = $val ->tone_3;
-            $tack_1 = $val->tack_1;
-            $tack_2 = $val->tack_2;
-            $tack_3 = $val->tack_3;
-            $pass_rate_1 = $val ->pass_rate_1;
-            $pass_rate_2 = $val ->pass_rate_2;
-            $pass_rate_3 = $val ->pass_rate_3;
-            $pass_trough_1 = $val ->pass_trough_1;
-            $pass_trough_2 = $val ->pass_trough_2;
-            $pass_trough_3 = $val ->pass_trough_3;
-            $cycle_time_buffing_1= $val ->cycle_time_buffing_1;
-            $cycle_time_buffing_2= $val ->cycle_time_buffing_2;
-            $cycle_time_buffing_3= $val ->cycle_time_buffing_3;
-            $article_code_1 = $val->article_code_1;
-            $article_code_2 = $val->article_code_2;
-            $article_code_3 = $val->article_code_3;
-            $article_code_4 = $val->article_code_4;
-            $tone1 = $val->tone_d_1;
-            $tone2 = $val->tone_d_2;
-            $tone3 = $val->tone_d_3;
-            $tone4 = $val->tone_d_4;
-            $pos1 = $val->pos_1;
-            $pos2 = $val->pos_2;
-            $pos3 = $val->pos_3;
-            $pos4 = $val->pos_4;
-            $qty1 = $val->qty_1;
-            $qty2 = $val->qty_2;
-            $qty3 = $val->qty_3;
-            $qty4 = $val->qty_4;
-            $uom1 = $val->uom_1;
-            $uom2 = $val->uom_2;
-            $uom3 = $val->uom_3;
-            $uom4 = $val->uom_4;
-            $uom_con_1 = $val->uom_con_1;
-            $uom_con_2 = $val->uom_con_2;
-            $uom_con_3 = $val->uom_con_3;
-            $uom_con_4 = $val->uom_con_4;
+        //     $bom_code = $val->bom_code;
+        //     $num_revision = $val->num_revision;
+        //     $article_finish_good = $val->article_finish_good;
+        //     $article_raw_material = $val->article_raw_material;
+        //     $customer = $val->customer;
+        //     $group_of_material = $val->group_of_material;
+        //     $uom = $val->uom;
+        //     $part_no = $val->part_no;
+        //     $model = $val->model;
+        //     $note = $val->note;
+        //     $spray_booth_1 = $val->spray_booth_1;
+        //     $spray_booth_2 = $val->spray_booth_2;
+        //     $spray_booth_3 = $val->spray_booth_3;
+        //     $tone_1 = $val->tone_1;
+        //     $tone_2 = $val ->tone_2;
+        //     $tone_3 = $val ->tone_3;
+        //     $tack_1 = $val->tack_1;
+        //     $tack_2 = $val->tack_2;
+        //     $tack_3 = $val->tack_3;
+        //     $pass_rate_1 = $val ->pass_rate_1;
+        //     $pass_rate_2 = $val ->pass_rate_2;
+        //     $pass_rate_3 = $val ->pass_rate_3;
+        //     $pass_trough_1 = $val ->pass_trough_1;
+        //     $pass_trough_2 = $val ->pass_trough_2;
+        //     $pass_trough_3 = $val ->pass_trough_3;
+        //     $cycle_time_buffing_1= $val ->cycle_time_buffing_1;
+        //     $cycle_time_buffing_2= $val ->cycle_time_buffing_2;
+        //     $cycle_time_buffing_3= $val ->cycle_time_buffing_3;
+        //     $article_code_1 = $val->article_code_1;
+        //     $article_code_2 = $val->article_code_2;
+        //     $article_code_3 = $val->article_code_3;
+        //     $article_code_4 = $val->article_code_4;
+        //     $tone1 = $val->tone_d_1;
+        //     $tone2 = $val->tone_d_2;
+        //     $tone3 = $val->tone_d_3;
+        //     $tone4 = $val->tone_d_4;
+        //     $pos1 = $val->pos_1;
+        //     $pos2 = $val->pos_2;
+        //     $pos3 = $val->pos_3;
+        //     $pos4 = $val->pos_4;
+        //     $qty1 = $val->qty_1;
+        //     $qty2 = $val->qty_2;
+        //     $qty3 = $val->qty_3;
+        //     $qty4 = $val->qty_4;
+        //     $uom1 = $val->uom_1;
+        //     $uom2 = $val->uom_2;
+        //     $uom3 = $val->uom_3;
+        //     $uom4 = $val->uom_4;
+        //     $uom_con_1 = $val->uom_con_1;
+        //     $uom_con_2 = $val->uom_con_2;
+        //     $uom_con_3 = $val->uom_con_3;
+        //     $uom_con_4 = $val->uom_con_4;
             
-            $barisIsi = "<tr>
-                                <td>$bom_code</td>
-                                <td>$num_revision</td>
-                                <td>$article_finish_good</td>
-                                <td>$article_raw_material</td>
-                                <td>$customer</td>
-                                <td>$group_of_material</td>
-                                <td>$uom</td>
-                                <td>$part_no</td>
-                                <td>$model</td>
-                                <td>$note</td>
-                                <td>$spray_booth_1</td>
-                                <td>$spray_booth_2</td>
-                                <td>$spray_booth_3</td>
-                                <td>$tone_1</td>
-                                <td>$tone_2</td>
-                                <td>$tone_3</td>
-                                <td>$tack_1</td>
-                                <td>$tack_2</td>
-                                <td>$tack_3</td>
-                                <td>$pass_rate_1</td>
-                                <td>$pass_rate_2</td>
-                                <td>$pass_rate_3</td>
-                                <td>$pass_trough_1</td>
-                                <td>$pass_trough_2</td>
-                                <td>$pass_trough_3</td>
-                                <td>$cycle_time_buffing_1</td>
-                                <td>$cycle_time_buffing_2</td>
-                                <td>$cycle_time_buffing_3</td>
-                                <td>$article_code_1</td>
-                                <td>$article_code_2</td>
-                                <td>$article_code_3</td>
-                                <td>$article_code_4</td>
-                                <td>$tone1</td>
-                                <td>$tone2</td>
-                                <td>$tone3</td>
-                                <td>$tone4</td>
-                                <td>$pos1</td>
-                                <td>$pos2</td>
-                                <td>$pos3</td>
-                                <td>$pos4</td>
-                                <td>$qty1</td>
-                                <td>$qty2</td>
-                                <td>$qty3</td>
-                                <td>$qty4</td>
-                                <td>$uom1</td>
-                                <td>$uom2</td>
-                                <td>$uom3</td>
-                                <td>$uom4</td>
-                                <td>$uom_con_1</td>
-                                <td>$uom_con_2</td>
-                                <td>$uom_con_3</td>
-                                <td>$uom_con_4</td>
-                            </tr>";
+        //     $barisIsi = "<tr>
+        //                         <td>$bom_code</td>
+        //                         <td>$num_revision</td>
+        //                         <td>$article_finish_good</td>
+        //                         <td>$article_raw_material</td>
+        //                         <td>$customer</td>
+        //                         <td>$group_of_material</td>
+        //                         <td>$uom</td>
+        //                         <td>$part_no</td>
+        //                         <td>$model</td>
+        //                         <td>$note</td>
+        //                         <td>$spray_booth_1</td>
+        //                         <td>$spray_booth_2</td>
+        //                         <td>$spray_booth_3</td>
+        //                         <td>$tone_1</td>
+        //                         <td>$tone_2</td>
+        //                         <td>$tone_3</td>
+        //                         <td>$tack_1</td>
+        //                         <td>$tack_2</td>
+        //                         <td>$tack_3</td>
+        //                         <td>$pass_rate_1</td>
+        //                         <td>$pass_rate_2</td>
+        //                         <td>$pass_rate_3</td>
+        //                         <td>$pass_trough_1</td>
+        //                         <td>$pass_trough_2</td>
+        //                         <td>$pass_trough_3</td>
+        //                         <td>$cycle_time_buffing_1</td>
+        //                         <td>$cycle_time_buffing_2</td>
+        //                         <td>$cycle_time_buffing_3</td>
+        //                         <td>$article_code_1</td>
+        //                         <td>$article_code_2</td>
+        //                         <td>$article_code_3</td>
+        //                         <td>$article_code_4</td>
+        //                         <td>$tone1</td>
+        //                         <td>$tone2</td>
+        //                         <td>$tone3</td>
+        //                         <td>$tone4</td>
+        //                         <td>$pos1</td>
+        //                         <td>$pos2</td>
+        //                         <td>$pos3</td>
+        //                         <td>$pos4</td>
+        //                         <td>$qty1</td>
+        //                         <td>$qty2</td>
+        //                         <td>$qty3</td>
+        //                         <td>$qty4</td>
+        //                         <td>$uom1</td>
+        //                         <td>$uom2</td>
+        //                         <td>$uom3</td>
+        //                         <td>$uom4</td>
+        //                         <td>$uom_con_1</td>
+        //                         <td>$uom_con_2</td>
+        //                         <td>$uom_con_3</td>
+        //                         <td>$uom_con_4</td>
+        //                     </tr>";
                                     
-            $barisAll .= $barisIsi;
-        }; 
+        //     $barisAll .= $barisIsi;
+        // }; 
 
-        $barisAll = $barisHeader.$barisAll;
+        // $barisAll = $barisHeader.$barisAll;
               
-        $data['barisDetail']=$barisAll;       
+        // $data['barisDetail']=$barisAll;
+        $data['headers']=$headers;       
         return view('bom.exportToExcel', $data);
     }
 
