01078883a465bac76dd0b46d098a9da3bf56abda
Oki Hartanto <oki.hartanto@yogyagroup.com>
2026-05-25 08:32:41 +0700

    Perbaikan export ke excel penambahan kolom keterangan dan note

--- resources/views/receiving/index.blade.php
+++ resources/views/receiving/index.blade.php
@@ -203,7 +203,7 @@
       route:"{{ route('receiving.list.detail') }}",
       kolom:{!! $kolomDetail !!},
       type:'POST',
-      arrColPrint:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,25,26],
+      arrColPrint:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,25,26],
       columnDefs :[
         { width: '5%', targets: 0 },
         {
