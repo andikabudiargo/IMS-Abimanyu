f2d7023425bdbe6ae0c3328bf23e8c7d5f7a380c
Andika Budiargo <87258005+andikabudiargo@users.noreply.github.com>
2026-07-08 12:20:41 +0700

    Update index.blade.php

--- resources/views/bomReport/index.blade.php
+++ resources/views/bomReport/index.blade.php
@@ -125,7 +125,7 @@
       tableId:"detailedTable",
       route:"{{ route('bom.report.list') }}",
       kolom:{!! $kolom !!},
-      arrColPrint:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16],
+      arrColPrint:[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14],
       columnDefs :[
         { width: '5%', targets: 0 },
         {
