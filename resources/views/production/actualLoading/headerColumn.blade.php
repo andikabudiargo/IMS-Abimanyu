bcab927847cbb3e9211d11e540b028c99cc02243
Andika Budiargo <87258005+andikabudiargo@users.noreply.github.com>
2026-07-23 15:18:28 +0700

    Update headerColumn.blade.php

--- resources/views/production/actualLoading/headerColumn.blade.php
+++ resources/views/production/actualLoading/headerColumn.blade.php
@@ -2,11 +2,8 @@
     <div class="col-md-4 col-12 d-none d-md-block">
         <div class="form-group"><label class="d-none d-md-block font-weight-bold">Article Code</label></div>
     </div>
-    <div class="col-md-1 col-12 d-none d-md-block">
+    <div class="col-md-2 col-12 d-none d-md-block">
         <div class="form-group"><label class="d-none d-md-block font-weight-bold text-right">RM Fresh</label></div>
-    </div>
-    <div class="col-md-1 col-12 d-none d-md-block">
-        <div class="form-group"><label class="d-none d-md-block font-weight-bold text-right">RM Repaint</label></div>
     </div>
     <div class="col-md-1 col-12 d-none d-md-block">
         <div class="form-group"><label class="d-none d-md-block font-weight-bold">QTY</label></div>
