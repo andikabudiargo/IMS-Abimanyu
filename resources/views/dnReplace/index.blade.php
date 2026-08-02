28b8a709f5252ceb503bfe3375abe94e88236819
Andika Budiargo <87258005+andikabudiargo@users.noreply.github.com>
2026-08-02 10:14:24 +0700

    Update index.blade.php

--- resources/views/dnReplace/index.blade.php
+++ resources/views/dnReplace/index.blade.php
@@ -191,11 +191,11 @@
       route:"{{ route('dnReplace.list.detail') }}",
       kolom:{!! $kolomDetail !!},
       type:'POST',
-      arrColPrint:[0,1,2,3,4,5,6,7,8,9,10,11,12],
+      arrColPrint:[0,1,2,3,4,5,6,7,8,9,10,11,12,13],
       columnDefs :[
         { width: '5%', targets: 0 },
         {
-          targets: [ 8,9,10 ],
+          targets: [ 9,10,11 ],
           render: $.fn.dataTable.render.number(',', '.',2, ''),
           className: "text-right"
         },
