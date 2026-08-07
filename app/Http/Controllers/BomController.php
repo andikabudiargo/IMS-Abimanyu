5af783329ffeab6d14dfcde054ad23fce3d7516f
Andika Budiargo <87258005+andikabudiargo@users.noreply.github.com>
2026-08-07 16:03:45 +0700

    Update BomController.php

--- app/Http/Controllers/BomController.php
+++ app/Http/Controllers/BomController.php
@@ -719,7 +719,7 @@ if (!empty($articleCodeRm)) {
     $statusPr = ['NEW','VALIDATE','APPROVED','RECEIVED','DELETED','CLOSED','REVISED','DECLINE'];
     $data['statusBom'] = $statusPr[$data['headers'][0]->status-1];
 
-    $data['arrSprayBooth'] = ['sb1'=>'Spraybooth 1','sb1a'=>'Spraybooth 1 A','sb1b'=>'Spraybooth 1 B','sb1c'=>'Spraybooth 1 C','sb2'=>'Spraybooth 2','sb2a'=>'Spraybooth 2 A','sb2b'=>'Spraybooth 2 B','sb2c'=>'Spraybooth 2 C','sb3'=>'Spraybooth 3','sb3a'=>'Spraybooth 3 A','sb3b'=>'Spraybooth 3 B','sb3c'=>'Spraybooth 3 C','sb4'=>'Spraybooth 4','sb4a'=>'Spraybooth 4 A','sb4b'=>'Spraybooth 4 B','sb4c'=>'Spraybooth 4 C','sbtoto'=>'Toto'];
+    $data['arrSprayBooth'] = ['sb1'=>'Spraybooth 1','sb1a'=>'Spraybooth 1 A','sb1b'=>'Spraybooth 1 B','sb1c'=>'Spraybooth 1 C','sb2'=>'Spraybooth 2','sb2a'=>'Spraybooth 2 A','sb2b'=>'Spraybooth 2 B','sb2c'=>'Spraybooth 2 C','sb3'=>'Spraybooth 3','sb3a'=>'Spraybooth 3 A','sb3b'=>'Spraybooth 3 B','sb3c'=>'Spraybooth 3 C','sb4'=>'Spraybooth 4','sb4a'=>'Spraybooth 4 A','sb4b'=>'Spraybooth 4 B','sb4c'=>'Spraybooth 4 C','sb5'=>'Spraybooth 5','sb5a'=>'Spraybooth 5 A','sb5b'=>'Spraybooth 5 B','sb5c'=>'Spraybooth 5 C','sbtoto'=>'Toto'];
 
     $data['arrTone'] = ['t1'=>'Tone 1','t2'=>'Tone 2','t3'=>'Tone 3','t4'=>'Tone 4'];
 
