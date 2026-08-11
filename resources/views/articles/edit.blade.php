b452e86ca397db9f6f67a711e5a09a9055cf9541
Andika Budiargo <87258005+andikabudiargo@users.noreply.github.com>
2026-08-11 08:37:00 +0700

    Update edit.blade.php

--- resources/views/articles/edit.blade.php
+++ resources/views/articles/edit.blade.php
@@ -107,7 +107,7 @@
                             </div>
                             <div class="form-group col-md-6">
                                 <label class="form-label" for="coa">Chart of Account (CoA)</label>
-                                <select class="select2 form-control" id="coa" name="coa" required>
+                                <select class="select2 form-control" id="coa" name="coa">
                                     <option value=""></option>
                                    @foreach($accounts as $val)
                                                     <option value="{{ $val->account }}" {{ old('coa') == $val->account ? 'selected' : '' }}>{{ $val->account}} - {{ $val->description }}</option>
