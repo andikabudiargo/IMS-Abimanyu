3827323ac961c1d4b76118d94778f5746bdd7cc2
Andika Budiargo <87258005+andikabudiargo@users.noreply.github.com>
2026-08-12 13:37:09 +0700

    Update printV2.blade.php

--- resources/views/invoice/printV2.blade.php
+++ resources/views/invoice/printV2.blade.php
@@ -170,12 +170,14 @@
             margin-left : 1.4mm;
         }
 
-      .sub_div_tengah {
+     .sub_div_tengah {
     height : 524px;
     width  : 803px;
     position: absolute;
     margin-left : 1.4mm;
     bottom: 355px;
+    outline: 2px solid #0c0c0c;        /* ← garis luar container, tidak terpotong overflow */
+    outline-offset: -1px;              /* ← masuk ke dalam supaya tidak terpotong di kiri/kanan */
 }
     </style>
 </head>
