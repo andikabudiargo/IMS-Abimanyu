1453794e7dcce8195466cc3c5b4132b5972df6fb
Andika Budiargo <87258005+andikabudiargo@users.noreply.github.com>
2026-09-16 09:36:05 +0700

    Update sidepanel.blade.php

--- resources/views/layouts/sidepanel.blade.php
+++ resources/views/layouts/sidepanel.blade.php
@@ -862,12 +862,10 @@
               </a>
             </li>
             @endcan
-
-             
            
               <li><a class="d-flex align-items-center" href="#"><i data-feather="circle"></i><span class="menu-item text-truncate" data-i18n="Form">Form</span></a>
                 <ul class="menu-content">
-                    <li class="{{ \Request::segment(1) == 'changeRequest'  ? 'active' : '' }}"><a class="d-flex align-items-center" href="#"><span class="menu-item text-truncate" data-i18n="changeRequest">Change Request</span></a>
+                    <li class="{{ \Request::segment(1) == 'formChangeRequest'  ? 'active' : '' }}"><a class="d-flex align-items-center" href="{{ route('formChangeRequest.index') }}"><span class="menu-item text-truncate" data-i18n="changeRequest">Change Request</span></a>
                     </li>
                     
                 </ul>
@@ -955,12 +953,6 @@
             @endcan
 
           </ul>
-        </li>
-        <li class=" {{ \Request::segment(1) == 'formChangeRequest' ? 'active' : '' }} nav-item">
-          <a class="d-flex align-items-center" href="{{ route('formChangeRequest.index') }}">
-            <i data-feather="edit-3"></i>
-            <span class="menu-title text-truncate" data-i18n="Form Elements">Form Change Request</span>
-          </a>
         </li>
         <li class=" {{ in_array(\Request::segment(1), ['logActivity']) ? 'active' : '' }} nav-item">
           <a class="d-flex align-items-center" href="javascript:void(0);">
