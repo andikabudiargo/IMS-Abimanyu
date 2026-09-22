cdfd655001ec5656ca77b9fc3dba7566bc228d41
Andika Budiargo <87258005+andikabudiargo@users.noreply.github.com>
2026-09-22 15:16:18 +0700

    Update sidepanel.blade.php

--- resources/views/layouts/sidepanel.blade.php
+++ resources/views/layouts/sidepanel.blade.php
@@ -200,6 +200,7 @@
               @can('salesOrder-index')
                     <li class="{{ \Request::is(['deliveryReportAcc'])  ? 'active' : '' }}">
                       <a class="d-flex align-items-center" href="{{ route('delivery.report.acc') }}">
+                        <i data-feather="circle"></i>
                         <span class="menu-item text-truncate" data-i18n="Dn Report Acc">DN Report</span>
                       </a>
                     </li>
